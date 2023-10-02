b53e50913709f871243d471e890c5efefbbdcd4b
Nazmul Hridoy <hridoyn75@gmail.com>
2023-10-02 13:36:11 +0600

    version 0.1.1

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 "use client";
+import Loader from "@/components/loader";
 import axios from "axios";
 import { useState } from "react";
 
@@ -94,25 +95,7 @@ export default function Home() {
         )}
 
         {loading && (
-          <div className=" flex justify-center items-center h-[250px]">
-            <div className="loader">
-              <svg viewBox="0 0 80 80">
-                <circle id="test" cx="40" cy="40" r="32"></circle>
-              </svg>
-            </div>
-
-            <div className="loader triangle">
-              <svg viewBox="0 0 86 80">
-                <polygon points="43 8 79 72 7 72"></polygon>
-              </svg>
-            </div>
-
-            <div className="loader">
-              <svg viewBox="0 0 80 80">
-                <rect x="8" y="8" width="64" height="64"></rect>
-              </svg>
-            </div>
-          </div>
+          <Loader />
         )}
       </div>
     </main>
